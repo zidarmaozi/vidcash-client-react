809020dee94da0e9b6a22a422fe68899d11a0764
dhanuprys <dhanuprys@gmail.com>
2025-10-15 08:21:02 +0800

    fix: spacing

--- src/components/home/VideoSection.tsx
+++ src/components/home/VideoSection.tsx
@@ -42,7 +42,7 @@ export default function VideoSection() {
             />
 
             <div className="max-w-7xl mx-auto">
-                <div className="px-4 sm:px-6 lg:px-8 mt-8">
+                <div className="lg:px-8 lg:mt-8">
                     <VideoInfo 
                         videoTitle={videoTitle}
                         videoId={videoId}
